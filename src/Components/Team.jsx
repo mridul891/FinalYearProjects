a12220c610084363095e1f70ce10be7005506529
Mridul Pandey <pandeym891@gmail.com>
2024-11-14 23:10:01 +0530

    minor

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -1,17 +1,18 @@
 import Card from "./Card";
 import logo from "../assets/c1.png";
-import logo2 from '../assets/c2.png'
-import logo3 from '../assets/c3.png'
-import logo4 from '../assets/c4.png'
-import logo5 from '../assets/c5.png'
-import logo6 from '../assets/c6.png'
+import logo2 from "../assets/c2.png";
+import logo3 from "../assets/c3.png";
+import logo4 from "../assets/c4.png";
+import logo5 from "../assets/c5.png";
+import logo6 from "../assets/c6.png";
 
 const Team = () => {
   return (
-    <div className="lg:px-[18rem] lg:flex lg:flex-col lg:justify-center lg:items-center" id="team">
-      <h1 className="text-3xl font-semibold ">
-        Let&apos;s Meet Our Team
-      </h1>
+    <div
+      className="lg:px-[18rem] lg:flex lg:flex-col lg:justify-center lg:items-center"
+      id="team"
+    >
+      <h1 className="text-3xl font-semibold ">The People Who Make It Happen</h1>
 
       <div className="grid grid-col-1 gap-15 md:grid-cols-2 lg:grid-cols-3 md:ml-20 ">
         <Card
@@ -42,7 +43,7 @@ const Team = () => {
         <Card
           src={logo}
           name={"Anand Tomar"}
-          position={"Full Stack Developer"}
+          position={"Machine Learning Engineer"}
         />
       </div>
     </div>
