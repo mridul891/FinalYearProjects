88b92a451dc01a33e9af9bfab3673819621a4179
Mridul Pandey <pandeym891@gmail.com>
2024-11-14 19:55:31 +0530

    Creation of Signup Done

--- src/Components/Signin.jsx
+++ src/Components/Signin.jsx
@@ -1,29 +1,48 @@
-import { useForm } from "react-hook-form";
-
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { useState } from "react";
 import { Label } from "./ui/label";
+import { Link } from "react-router-dom";
 
 export function Signin() {
-  // ...
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
-    <div className="w-full h-full bg-black flex flex-1 ">
-      <form action="" className="text-white">
-        <Label>Username</Label>
-        <input placeholder="shadcn" className="text-black"/>
-        <Label>Password</Label>
-        <input placeholder="shadcn" className="text-black"/>
-      </form>
+    <div className="w-full bg-black text-white h-full flex justify-center items-center ">
+      <div className=" p-10 flex flex-col justify-center w-full gap-2 lg:gap-4 lg:w-[40%] ">
+        <div>
+          <h1 className="text-3xl font-bold text-center mb-10 lg:text-left">
+            Sign In
+          </h1>
+        </div>
+        <div className="flex flex-col gap-3">
+          <Label className="text-2xl">Username</Label>
+          <input
+            placeholder="JohnDoe"
+            className="text-md rounded-xl px-5 py-2 font-semibold text-black"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Label className="text-2xl">Password</Label>
+          <input
+            placeholder="JohnDoe@123"
+            className="text-md rounded-xl px-5 py-2 font-semibold text-black"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <button className="bg-white text-black w-[10rem] rounded-xl text-lg py-2 font-semibold ">
+            Submit
+          </button>
+        </div>
+        <div>
+          <p className="font-semibold">
+            Doesn&apos;t have an account{" "}?
+            <span className="text-blue-400 ml-3">
+              <Link to="/signup">Register</Link>
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
