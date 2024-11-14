da68d2b2c274fc9abf54635052930c3e6a79ee90
Mridul Pandey <pandeym891@gmail.com>
2024-11-14 20:01:34 +0530

    Done with signup Page and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./Components/LandingPage";
-import {Signin} from "./Components/Signin";
-import Signup from "./Components/Signup";
+import { Signin } from "./Components/Signin";
+import { Signup } from "./Components/Signup";
+import Dashboard from "./Components/Dashboard";
 
 const App = () => {
   return (
     <div className="h-screen w-screen bg-[#FEFBFB]">
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { useState } from "react";
+import { Label } from "./ui/label";
+import { Link } from "react-router-dom";
 
-const Signup = () => {
+
+
+export function Signup() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [uniqueId , setUniqueId] =useState("")
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="w-full bg-black text-white h-full flex justify-center items-center ">
+      <div className=" p-10 flex flex-col justify-center w-full gap-2 lg:gap-4 lg:w-[40%] ">
+        <div>
+          <h1 className="text-3xl font-bold text-center mb-10 lg:text-left">
+            Sign Up
+          </h1>
+        </div>
+        <div className="flex flex-col gap-3">
+          <Label className="text-2xl">Username</Label>
+          <input
+            placeholder="JohnDoe"
+            className="text-md rounded-xl px-5 py-2 font-semibold text-black"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Label className="text-2xl">Password</Label>
+          <input
+            placeholder="JohnDoe@123"
+            className="text-md rounded-xl px-5 py-2 font-semibold text-black"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+           <Label className="text-2xl">Uniqueid</Label>
+          <input
+            placeholder="b12de"
+            className="text-md rounded-xl px-5 py-2 font-semibold text-black"
+            onChange={(e) => setUniqueId(e.target.value)}
+          />
+        </div>
 
-export default Signup
+        <div>
+          <button className="bg-white text-black w-[10rem] rounded-xl text-lg py-2 font-semibold ">
+            Submit
+          </button>
+        </div>
+        <div>
+          <p className="font-semibold">
+            Already Have an account ?
+            <span className="text-blue-400 ml-3">
+              <Link to="/signin">Login</Link>
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
