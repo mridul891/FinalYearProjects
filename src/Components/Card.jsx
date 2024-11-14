e94c12a5524a961a8ff692ce27db5b4b09244e2c
Mridul Pandey <pandeym891@gmail.com>
2024-11-14 22:39:28 +0530

    Made responsive

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,13 @@
 
 const Card = ({ src, name, position }) => {
   return (
-    <div className="flex flex-col lg:px-10 pt-10 justify-center ">
+    <div className="flex flex-row item-center justify-center gap-10 lg:flex-col lg:px-10 pt-10 lg:gap-2 ">
       {/* character image */}
       <div className="bg-yellow-400 rounded-full overflow-hidden w-[5rem] ">
         <img src={src} alt="" className="h-[5rem]" />
       </div>
       {/* Person information */}
-      <div className="mt-3">
+      <div className="mt-3 w-[10rem] lg:w-[20rem]">
         <h1 className="font-semibold">{name}</h1>
         <p className="text-violet-700">{position}</p>
       </div>
