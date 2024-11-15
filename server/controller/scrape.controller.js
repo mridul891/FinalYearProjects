ec929b9d78c6558d171f8588831e3366b417a497
Mridul Pandey <pandeym891@gmail.com>
2024-11-15 16:47:03 +0530

    DashBoard Creation

--- server/controller/scrape.controller.js
+++ server/controller/scrape.controller.js
@@ -1,5 +1,6 @@
 export const scrapeController = async (req, res) => {
-    
+  const body =req.body 
+  
   res.json({
     message: "THe scrape is working",
   });
--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,14 +1,41 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const [isLoaded, setLoaded] = useState(false);
+
+  const getData = async () => {
+    const response = await axios.get(
+      "https://webscrapper-bmoc.onrender.com/info"
+    );
+
+    setData(response.data);
+    setLoaded(true);
+  };
+
   useEffect(() => {
     if (!localStorage.getItem("sihToken")) {
       navigate("/signin");
     }
-  });
-  return <div>Dashboard</div>;
+    getData();
+  }, [navigate]);
+
+  return (
+    <div>
+      {isLoaded &&
+        data.map((elem, index) => {
+          return (
+            <>
+              <h1 key={index}>{elem.title}</h1>
+              <hr className="h-3 bg-black"></hr>
+            </>
+          );
+        })}
+    </div>
+  );
 };
 
 export default Dashboard;
