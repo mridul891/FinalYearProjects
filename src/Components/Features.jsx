f38a53f8a43c10dadcdf5749f1025d5f2b69a199
Mridul Pandey <pandeym891@gmail.com>
2024-11-15 08:12:38 +0530

    Optimization in the backend

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -1,17 +1,15 @@
-import Box from "./Box";
-import Card from "./Card";
-import img1 from "../assets/reading.png";
-import img2 from "../assets/bot.png";
-import img3 from "../assets/model.png";
-
 const Features = () => {
   return (
-    <div className="w-full flex justify-center items-center flex-col mt-10 md:p-1" id="features">
+    <div
+      className="w-full flex justify-center items-center flex-col mt-10 md:p-1"
+      id="features"
+    >
       {/* Title */}
-      <div className="text-4xl font-bold text-center mb-8">Features of RescueNet</div>
+      <div className="text-4xl font-bold text-center mb-8">
+        Features of RescueNet
+      </div>
 
       {/* Feature Boxes */}
-      
     </div>
   );
 };
